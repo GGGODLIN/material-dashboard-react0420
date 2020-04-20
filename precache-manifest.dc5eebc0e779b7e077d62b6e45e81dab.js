9bc3c7e9956d84bdf2af92304c313de1984e1e93
GGGODLIN <qwe70301@gmail.com>
2020-04-20 16:18:11 +0800

    Updates

---
+++ precache-manifest.dc5eebc0e779b7e077d62b6e45e81dab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e491ed2c50f4b009fb787e599f550c54",
+    "url": "/material-dashboard-react0420/index.html"
+  },
+  {
+    "revision": "7a652c5f26f83aa00af3",
+    "url": "/material-dashboard-react0420/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "7fedac8cbe838a53b7ad",
+    "url": "/material-dashboard-react0420/static/css/main.0fa7ec49.chunk.css"
+  },
+  {
+    "revision": "7a652c5f26f83aa00af3",
+    "url": "/material-dashboard-react0420/static/js/2.40f03d19.chunk.js"
+  },
+  {
+    "revision": "7fedac8cbe838a53b7ad",
+    "url": "/material-dashboard-react0420/static/js/main.d72537cf.chunk.js"
+  },
+  {
+    "revision": "54803cf626f6230e1f57",
+    "url": "/material-dashboard-react0420/static/js/runtime~main.b82d6e16.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/material-dashboard-react0420/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react0420/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react0420/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react0420/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react0420/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
